0e74ceef01a1ea673008a31c4ac030315cf66cd3
erik djamgarian <djamgarian0702@gmail.com>
2018-01-23 13:37:25 +0100

    more Closures

--- JSFunctions2.js
+++ JSFunctions2.js
@@ -38,11 +38,7 @@ game();
   console.log(score >= 5);
 })();
 
-  function (goodLuck) {
-    var score = Math.random() * 10;
-    console.log(score >= 5 - goodLuck);
 
-  }(5);
 
 
 function retirement(retirementAge) {
@@ -55,3 +51,45 @@ function retirement(retirementAge) {
 
 var retirementUS = retirement(66);
 retirementUS(1990);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementGermany(1990);
+retirementUS(1990);
+retirementIceland(1990);
+
+
+//retirement(66) (1990);
+
+/*
+function interviewQuestion(job) {
+  if (job === 'designer') {
+    return function(name) {
+      console.log(name + ', can you please explaing what UX design is?');
+    }
+  } else if (job === 'teacher') {
+    return function(name) {
+      console.log('What subject do you teach, ' + name + '?');
+    }
+  } else {
+    return function(name) {
+      console.log('Hello ' + name + ', what do you do?');
+    }
+  }
+}
+*/
+
+function interviewQuestion(job) {
+  return function(name) {
+    if (job === 'designer') {
+      console.log(name + ', can you please explain what UX design is?');
+    } else if (job === 'teacher')
+    {
+      console.log('What subject do you teach, ' + name + '?');
+    } else {
+      console.log('Hello' + name + ', what do you do?');
+    }
+  }
+}
+
+interviewQuestion('teacher')('John');
